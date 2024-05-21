40f3557a27e70bae8a711b0208f77a0d6de68e91
Gibson431 <timmaxgibson@gmail.com>
2024-05-21 15:53:08 +1000

    mutexes and tasks

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 #![no_std]
 #![no_main]
 
-use defmt as _;
+use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio;
-use embassy_rp::peripherals::USB;
-use embassy_rp::usb::{Driver, InterruptHandler};
-use embassy_time::Timer;
-use gpio::{Level, Output};
-use {defmt_rtt as _, panic_probe as _};
+use embassy_rp::i2c::{self, Config};
+use embassy_rp::peripherals::{I2C1, USB};
+use embassy_rp::usb::{self, Driver};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_time::{Duration, Ticker, Timer};
+// use embedded_hal_async::i2c::I2c;
+use gpio::{AnyPin, Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
+type LedType = Mutex<ThreadModeRawMutex, Option<Output<'static>>>;
+static LED: LedType = Mutex::new(None);
+
 bind_interrupts!(struct Irqs {
-    USBCTRL_IRQ => InterruptHandler<USB>;
+    USBCTRL_IRQ => usb::InterruptHandler<USB>;
+    I2C1_IRQ => i2c::InterruptHandler<I2C1>;
 });
 
 #[embassy_executor::task]
@@ -21,20 +28,43 @@ async fn logger_task(driver: Driver<'static, USB>) {
     embassy_usb_logger::run!(1024, log::LevelFilter::Debug, driver);
 }
 
+#[embassy_executor::task(pool_size = 2)]
+async fn heartbeat(name: &'static str, delay: Duration) {
+    let mut ticker = Ticker::every(delay);
+    loop {
+        log::info!("{name} heartbeat");
+        ticker.next().await;
+    }
+}
+
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(spawner: Spawner) -> !{
     let p = embassy_rp::init(Default::default());
     let driver = Driver::new(p.USB, Irqs);
     spawner.spawn(logger_task(driver)).unwrap();
-    let mut led = Output::new(p.PIN_25, Level::Low);
+    let led = Output::new(AnyPin::from(p.PIN_25), Level::High);
+
+    // inner scope is so that once the mutex is written to, the MutexGuard is dropped, thus the
+    // Mutex is released
+    {
+        *(LED.lock().await) = Some(led);
+    }
+
+    unwrap!(spawner.spawn(heartbeat("task1", Duration::from_secs(3))));
+    unwrap!(spawner.spawn(heartbeat("task2", Duration::from_secs(5))));
+
+    let sda = p.PIN_14;
+    let scl = p.PIN_15;
+    let mut _i2c = i2c::I2c::new_async(p.I2C1, scl, sda, Irqs, Config::default());
 
     loop {
-        log::info!("led on!");
-        led.set_high();
-        Timer::after_secs(1).await;
-
-        log::debug!("led off!");
-        led.set_low();
+        {
+            let mut led_unlocked = LED.lock().await;
+            if let Some(pin_ref) = led_unlocked.as_mut() {
+                pin_ref.toggle();
+                log::info!("led toggled!");
+            }
+        }
         Timer::after_secs(1).await;
     }
 }
